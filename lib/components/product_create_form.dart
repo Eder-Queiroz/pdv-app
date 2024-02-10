41418bab6770792e29ce555cc708f79bea568b50
Eder-Queiroz <eder.queiroz@estudante.iftm.edu.br>
2024-02-10 20:26:31 -0300

    Feat: product_create_form

--- lib/components/product_create_form.dart
+++ lib/components/product_create_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../data/category_data.dart';
+import '../model/category.dart';
 
 class ProductCreateForm extends StatefulWidget {
   const ProductCreateForm({super.key});
@@ -8,10 +10,156 @@ class ProductCreateForm extends StatefulWidget {
 }
 
 class _ProductCreateFormState extends State<ProductCreateForm> {
+  final _nameController = TextEditingController();
+  final _barcodeController = TextEditingController();
+  final _stockUnitController = TextEditingController();
+  final _priceController = TextEditingController();
+  final _costPriceController = TextEditingController();
+
+  Category _selectedCategory = categoryData[0];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Formulário produto'),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: Card(
+        elevation: 5,
+        child: SingleChildScrollView(
+          child: Padding(
+              padding: EdgeInsets.only(
+                top: 10,
+                right: 10,
+                left: 10,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: "Nome",
+                    ),
+                    controller: _nameController,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            labelText: "Cod. Barras",
+                          ),
+                          keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          controller: _barcodeController,
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            labelText: "Un. Estoque",
+                          ),
+                          keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          controller: _stockUnitController,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            labelText: "Preço",
+                          ),
+                          keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          controller: _priceController,
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            labelText: "Preço de Custo",
+                          ),
+                          keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true,
+                          ),
+                          controller: _costPriceController,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 10,
+                        bottom: 10,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Categoria",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          DropdownButton<Category>(
+                            isExpanded: true,
+                            alignment: Alignment.center,
+                            menuMaxHeight:
+                                MediaQuery.of(context).size.height * 0.3,
+                            elevation: 4,
+                            value: _selectedCategory,
+                            items: categoryData.map<DropdownMenuItem<Category>>(
+                              (Category category) {
+                                return DropdownMenuItem<Category>(
+                                  value: category,
+                                  child: Text(category.name),
+                                );
+                              },
+                            ).toList(),
+                            onChanged: (Category? newValue) {
+                              setState(() {
+                                _selectedCategory = newValue!;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                        ),
+                        child: const Text(
+                          "Criar Produto",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
+        ),
+      ),
     );
   }
 }

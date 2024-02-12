71beb3a62c826f010f0b5ed80c095ad2d124d087
Eder-Queiroz <eder.queiroz@estudante.iftm.edu.br>
2024-02-12 15:14:11 -0300

    Fix: add padding in category_create_form

--- lib/components/category_create_form.dart
+++ lib/components/category_create_form.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class CategoryCreateForm extends StatefulWidget {
   void Function(String) submitForm;
 
-  CategoryCreateForm({super.key,required this.submitForm});
+  CategoryCreateForm({super.key, required this.submitForm});
 
   @override
   _CategoryCreateFormState createState() => _CategoryCreateFormState();
@@ -15,32 +15,39 @@ class _CategoryCreateFormState extends State<CategoryCreateForm> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: 10,
+          right: 10,
+          left: 10,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
             TextField(
-              decoration: InputDecoration(
-                labelText: "Nome da Categoria"
-              ),
+              decoration: InputDecoration(labelText: "Nome da Categoria"),
               controller: _valueController,
               onSubmitted: (_) => widget.submitForm(_valueController.text),
             ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(onPressed: (){
-                widget.submitForm(_valueController.text);
-              },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                  ),
-                  child: Text(
-                "Criar Categoria",
-                style: TextStyle(color: Colors.white),
-              ))
-            ],
-          )
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      widget.submitForm(_valueController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                    ),
+                    child: Text(
+                      "Criar Categoria",
+                      style: TextStyle(color: Colors.white),
+                    ))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
